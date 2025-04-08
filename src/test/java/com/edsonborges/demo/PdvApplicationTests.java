44579a5c26117c50ff2f45df2a637a50970c6613
EdysonBorges <edysonborges@gmail.com>
2025-04-08 20:45:12 -0300

    Criação do projeto pdv

---
+++ src/test/java/com/edsonborges/demo/PdvApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsonborges.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdvApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
